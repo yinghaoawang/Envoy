f35dfe91ce601104c429acac1ca105dcce58aa47
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-04-15 01:28:44 -0700

    fix user not logged in on register

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,6 +1,5 @@
 export {};
 const { prisma } = require('../helpers/prismaHelper');
-const { isNotAuthenticated } = require('./routeMiddlewares');
 
 const { passport } = require('../helpers/passportHelper');
 const express = require('express');
@@ -17,27 +16,29 @@ router.post(
   }
 );
 
-router.post(
-  '/register',
-  isNotAuthenticated,
-  async (req: any, res: any, next: any) => {
-    try {
-      const salt = crypto.randomBytes(16);
-      const hashedPassword = crypto.pbkdf2Sync(
-        req.body.password,
-        salt,
-        310000,
-        32,
-        'sha256'
-      );
-      const user = { email: req.body.email, hashedPassword, salt };
-      await prisma.user.create({ data: user });
-      res.send('success');
-    } catch (error) {
-      return next(error);
-    }
+router.post('/register', async (req: any, res: any, next: any) => {
+  try {
+    const salt = crypto.randomBytes(16);
+    const hashedPassword = crypto.pbkdf2Sync(
+      req.body.password,
+      salt,
+      310000,
+      32,
+      'sha256'
+    );
+    const user = { email: req.body.email, hashedPassword, salt };
+    const userData = await prisma.user.create({ data: user });
+    passport.serializeUser(userData, (err: any, user: any) => {
+      if (err) {
+        return next(err);
+      }
+      req.session.passport = { user };
+      res.send(user);
+    });
+  } catch (error) {
+    return next(error);
   }
-);
+});
 
 router.post('/logout', async (req: any, res: any, next: any) => {
   req.logout(function (err: any) {
