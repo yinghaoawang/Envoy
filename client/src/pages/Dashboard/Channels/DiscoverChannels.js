edaee168c99d7b14a10582efe40617d3dec35c57
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-05-15 18:38:23 -0700

    update ui: discover channels is smoother, use flex instead of table

--- client/src/pages/Dashboard/Channels/DiscoverChannels.js
+++ client/src/pages/Dashboard/Channels/DiscoverChannels.js
@@ -32,30 +32,60 @@ const ChannelListItem = (props) => {
   };
 
   return (
-    <tr>
-      <td>{channel.id}</td>
-      <td>{channel.name}</td>
-      <td>{channel.owner.displayName}</td>
-      <td>{channel.users.length}</td>
-      <td>
-        <Moment fromNow={true}>{channel.createdAt}</Moment>
-      </td>
-      <td>
-        {containsUser || userJoined ? (
-          <button disabled={true} className='btn btn-secondary'>
-            Joined
-          </button>
-        ) : (
-          <button
-            disabled={loading}
-            onClick={onClickJoinChannel}
-            className='btn btn-success'
-          >
-            Join
-          </button>
-        )}
-      </td>
-    </tr>
+    <div className='bg-gray-800 p-3'>
+      <div className='d-flex justify-content-between'>
+        <div className='d-flex gap-4'>
+          <div className='d-flex'>
+            {channel.profileImgUrl ? (
+              <img
+                src={channel.profileImgUrl}
+                className='rounded-circle avatar me-3'
+                alt='avatar 3'
+                style={{ width: '60px', height: '60px' }}
+              />
+            ) : (
+              <div
+                className='rounded-circle avatar me-3'
+                style={{
+                  width: '60px',
+                  height: '60px',
+                  backgroundColor: '#000022'
+                }}
+              >
+                <div className='d-flex justify-content-center align-items-center h-100 text-white user-select-none'>{channel.name?.[0]?.toUpperCase()}</div>
+              </div>
+            )}
+            <div className='my-auto' style={{ maxWidth: '100px' }}>
+              <div className='fw-bold text-truncate'>{channel.name}</div>
+              <div className='text-muted text-truncate'>{channel.users.length} users</div>
+            </div>
+          </div>
+          <div className='my-auto text-truncate' style={{width: '100px' }}>
+            {channel.owner.displayName}
+          </div>
+          <div className='my-auto'>
+            Created <Moment fromNow={true}>{channel.createdAt}</Moment>
+          </div>
+        </div>
+        <div className='my-auto'>
+          <div style={{ width: '100px', height: '45px' }}>
+            {containsUser || userJoined ? (
+              <button disabled={true} className='btn btn-secondary w-100 h-100'>
+                Joined
+              </button>
+            ) : (
+              <button
+                disabled={loading}
+                onClick={onClickJoinChannel}
+                className='btn btn-success w-100 h-100'
+              >
+                Join
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
@@ -71,25 +101,17 @@ const DiscoverChannels = (props) => {
   }, []);
 
   return (
-    <div className='container p-4'>
-      <h1>Discover Channels</h1>
-      <table className='table text-white'>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Owner</th>
-            <th>Users</th>
-            <th>Created</th>
-            <th>Buttons</th>
-          </tr>
-        </thead>
-        <tbody>
+    <div className='container p-4 text-white bg-gray-950'>
+      <div>
+        <div className='mb-4'>
+          <h1>Discover Channels</h1>
+        </div>
+        <div className='text-white d-flex flex-column gap-3'>
           {channels.map((channel, idx) => (
             <ChannelListItem key={idx} channel={channel} />
           ))}
-        </tbody>
-      </table>
+        </div>
+      </div>
     </div>
   );
 };
